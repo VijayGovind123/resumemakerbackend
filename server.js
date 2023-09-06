15dcf63659f83964373469da7f269934e88286fd
Ande Kishore <120158003+VijayGovind123@users.noreply.github.com>
2023-09-06 17:12:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PDFDocument = require("pdfkit");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const puppeteer = require("puppeteer");
+//const puppeteer = require("puppeteer");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -88,65 +88,82 @@ app.post("/generate-sop", async (req, res) => {
       console.log("Rate limit exceeded. Generating custom letter...");
       pdfFilePath = generateCustomLetter(req.body);
     } else {
-      const prompt = `
-Create a professional resume in HTML format according to this job description:${jobdesc} with the following details :
-also add summary at the top according to given details
-Name: ${name} \n
-Email: ${email}  \n
-Phone: ${mobile} \n
-github link: ${github} \n
-linkedin link:${linkedin} \n
-Work Experience: ${experience} \n
-Education details : ${education} \n
-Skills: ${skills} \n
-Achievements:${achievements}\n
-Extra curricular activites:${extra}\n
-Competetive Programming:${comp}
-information about Projects: ${projects} \n
+      const prompt = `Generate a professional resume for applying for job of a job description : ${jobdesc} \nneatly with  appropriate spacing and points with the following sections:
 
-Design similar to this html code :
+      
+      Introduction : write the short introduction as a student with the use of details  given below using ai \n
+      Education:${education} \n
+      Experience: ${experience} \n
+      Projects: ${projects} \n 
+      Achievements: ${achievements} \n
+      Technical Skills: ${skills} \n
+      Competitive Programming: ${comp} \n
+      Extra-curricular Activities: ${extra}]\n
+      
+      Please include the delimiter "ABCDE" between each section for easy parsing and also modify the content professionally using AI.
+      Please dont include name and job description in the resume`;
 
-
-Design the resume with these specifications:
-
-Design a resume that is:
-1.Give blue color to every link and keep times new roman font.
-2.email,github,linkedin links should be in the same line and do not mention them as email , github,linkedin like that just keep the links.
-3.follow this order : name,email,github,linkedin -> summary ->education ->experience -> projects -> achievements -> skills -> competetive programming -> extracurriculars  
-
-4.seperate every section with a line .
-5.Professional and easy to read, using a sans-serif font like Roboto, 12pt for the main text and 14pt for the headings.
-Well-formatted, with 1.15 line spacing and text aligned to the left.
-Visually appealing, using a professional color scheme like black and blue, and plenty of whitespace.
-Customized to the applicant, by including relevant information such as their name, contact information, education, experience, and skills.
-
-6.give 40px padding from every side.
-7.Use bullet points to make it looking good wherever it is necessary
-8.Be clear and concise.  `;
       const completion = await openai.completions.create({
         prompt: prompt,
         max_tokens: 2000,
         model: "text-davinci-003",
         temperature: 1,
       });
-      const generatedText = completion.choices[0].text;
-      console.log(generatedText);
+      const resumeText = completion.choices[0].text;
+      console.log(resumeText);
+      const sections = resumeText.split("ABCDE");
+      console.log(sections.length);
+      console.log(sections);
 
-      (async () => {
-         const browser = await puppeteer.launch({
-            headless: "new",
-            ignoreDefaultArgs: ['--disable-extensions'],
-            executablePath: '/usr/bin/chromium-browser'
-           // this made it work for now
-        });
-        const page = await browser.newPage();
-        const htmlContent = `${generatedText}`; // Replace with your OpenAI HTML
-        await page.setContent(htmlContent);
-        await page.pdf({ path: "output.pdf", format: "A4" }); // You can specify the PDF format and output path
-        await browser.close();
-      })();
+      const doc = new PDFDocument({ margin: 50 });
+      const pdfFilePath = "output.pdf";
+      const pdfStream = fs.createWriteStream(pdfFilePath);
+      doc.pipe(pdfStream);
 
-      pdfFilePath = "output.pdf";
+      const fontPath = "sfpro.ttf"; // Replace with the path to your TTF font
+
+      // Set the font for the entire document
+      doc.font(fontPath);
+
+      // Create the custom letter content in a formal manner
+
+      doc.fontSize(16).text(`${name}`);
+      doc.moveDown(1);
+      doc.fontSize(12).text(` LinkedIn | `, {
+        continued: true,
+        link: linkedin,
+      });
+
+      // Add GitHub link
+      doc.fontSize(12).text(` Github |`, {
+        continued: true,
+        link: github,
+      });
+
+      // Add email link
+      doc
+        .fontSize(12)
+        .text(` ${email} |`, { continued: true, link: `mailto:${email}` });
+      doc
+        .fontSize(12)
+        .text(` ${mobile} `, { continued: true, link: `tel:${mobile}` });
+
+      doc.moveDown(2);
+      // Add phone number link
+
+      sections.forEach((section) => {
+        doc
+          .strokeColor("#000000")
+          .lineWidth(1)
+          .moveTo(50, doc.y)
+          .lineTo(550, doc.y)
+          .stroke();
+
+        doc.fontSize(12).text(section);
+        doc.moveDown(1);
+      });
+
+      doc.end();
       const mailOptions = {
         from: "shelbyltdx5@gmail.com",
         to: email,
@@ -154,7 +171,7 @@ Customized to the applicant, by including relevant information such as their nam
         text: `Dear ${name} ,Here is your resume . Have a Nice Day`,
         attachments: [
           {
-            filename: "sop.pdf",
+            filename: "resume.pdf",
             path: pdfFilePath,
             contentType: "application/pdf",
           },
